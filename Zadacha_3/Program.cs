2ff8abb4c1b3904bc0f30992597dc38d599fa233
Oleynik Andrey <andry44666@gmail.com>
2023-01-27 04:12:54 +0300

    Complited Zadacha_3

---
+++ Zadacha_3/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+Console.WriteLine("Enter numbers :");
+string strnumberA = Console.ReadLine();
+int numberA = Convert.ToInt32(strnumberA);
+if (numberA % 2 == 0)
+{
+    System.Console.WriteLine("entered number even");
+}
+else
+{
+    System.Console.WriteLine("entered number not even");
+}
