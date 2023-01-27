851fda3996e42567d8df276277b81df20967647a
Oleynik Andrey <andry44666@gmail.com>
2023-01-27 05:03:05 +0300

    Complited Zadacha_4

---
+++ Zadacha_4/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.Write("Enter number :");
+string strnuber = Console.ReadLine();
+int number = System.Convert.ToInt32(strnuber);
+int result = 1;
+while (result < number)
+{
+    System.Console.WriteLine(result + 1);
+    result +=2;
+}
